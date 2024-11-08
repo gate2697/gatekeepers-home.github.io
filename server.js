f6c97a5358793f957965ddca191e7a257b93f2f7
gate2697 <111462829+gate2697@users.noreply.github.com>
2024-11-08 02:30:59 +0000

    abcabc

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const crypto = require('crypto');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import crypto from 'crypto';
+
 
 const app = express();
 const PORT = process.env.PORT || 5304;
